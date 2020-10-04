a2ecce7b00f1af598c415bb6abc4a932ba809a36
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 19:42:29 +0200

    create die component

---
+++ src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Die extends Component {
+  render() {
+    return (
+      <div className="Die">
+        <i class="fas fa-dice-one"></i>
+      </div>
+    );
+  }
+}
+
+export default Die;
