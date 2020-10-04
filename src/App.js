32e084ff40580997c65500aeab2df9b7926a3605
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 19:34:06 +0200

    add roll dice component to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import RollDice from './RollDice';
 
 function App() {
   return (
     <div className="App">
-      <h1>App component</h1>
+      <RollDice />
     </div>
   );
 }
