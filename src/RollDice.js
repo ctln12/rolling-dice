90746dfde832be4f6dad80077b6caada8e900272
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 19:33:45 +0200

    create roll dice component

---
+++ src/RollDice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RollDice extends Component {
+  render() {
+    return (
+      <div className="RollDice">
+        <button>Roll Dice!</button>
+      </div>
+    );
+  }
+}
+
+export default RollDice;
