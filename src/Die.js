5fdc4b04f921fe31829f32fba3920b8f307fdc71
Cataline Baert <snuf_kin@yahoo.com>
2020-10-04 20:11:54 +0200

    change class to className

--- src/Die.js
+++ src/Die.js
@@ -6,7 +6,7 @@ class Die extends Component {
   render() {
     return (
       <div className="Die">
-        <i class="fas fa-dice-one"></i>
+        <i className="fas fa-dice-one"></i>
       </div>
     );
   }
