710089c1acc3d3c9aa8cb312739b176c21c80351
Cataline Baert <snuf_kin@yahoo.com>
2020-10-05 11:18:12 +0200

    render dice icon dynamic

--- src/Die.js
+++ src/Die.js
@@ -4,9 +4,10 @@ import './Die.css';
 
 class Die extends Component {
   render() {
+    const {number} = this.props;
     return (
       <div className="Die">
-        <i className="fas fa-dice-one"></i>
+        <i className={`fas fa-dice-${number}`}></i>
       </div>
     );
   }
