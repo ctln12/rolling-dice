1a43ba6b4c86db9b0eeea4d5c5123408e6c25a19
Cataline Baert <snuf_kin@yahoo.com>
2020-10-05 12:08:05 +0200

    get and use new isRolling prop

--- src/Die.js
+++ src/Die.js
@@ -4,10 +4,11 @@ import './Die.css';
 
 class Die extends Component {
   render() {
-    const {number} = this.props;
+    const {number, isRolling} = this.props;
     return (
       <div className="Die">
-        <i className={`fas fa-dice-${number}`}></i>
+        <i
+          className={`fas fa-dice-${number}${isRolling ? ' rolling' : ''}`}></i>
       </div>
     );
   }
